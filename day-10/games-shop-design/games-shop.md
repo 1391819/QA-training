c93aa19d6b7fb7e902420a419a412e63a3d8b7ed
1391819 <roberto.nacu@gmail.com>
2023-07-14 16:17:15 +0100

    repo restructuring + added sql file

---
+++ day-10/games-shop-design/games-shop.md
@@ -0,0 +1,119 @@
+# SQL Practice - Developing a database for a games shop
+
+## Instructions
+> Think about the type of data that a games shop would want to store in their database. From here, think about the tables that you might find in the games shop's database. What kinds of information will they store?
+
+- Customers
+- Products
+  - Games
+  - Consoles
+- Orders
+- Employees
+
+## Data we want to track
+- Game name
+- Customer
+  - Customer account?
+- Stock Level / Inventory 
+- Sales transaction
+- Genre
+- Game compatibility
+- Suppliers
+- Ratings / Reviews
+- Prices
+
+## Tables
+### Customers
+| Field | Type | Constraints |
+| ----- | ---- | ----------- |
+| customer_ID | INT | PRIMARY KEY |
+| email | VARCHAR(50) | NOT NULL |
+| phone_number | VARCHAR(50) |
+| pwd | binary(20) | NOT NULL |
+| salt | binary(20) | NOT NULL |
+
+```sql
+CREATE TABLE customers (
+  customer_ID INT PRIMARY KEY,
+  email VARCHAR(50) NOT NULL,
+  phone_number VARCHAR(50),
+  pwd BINARY(20) NOT NULL,
+  salt BINARY(20) NOT NULL
+);
+```
+    
+### Products
+| Field | Type | Constraints |
+| ----- | ---- | ----------- |
+| product_ID | INT | PRIMARY KEY |
+| product_name | VARCHAR(50) | NOT NULL |
+| FK_genre_ID | INT | FOREIGN KEY |
+| rating | INT | DEFAULT = 0 |
+| price | FLOAT | NOT NULL |
+| FK_supplier_ID | INT | FOREIGN KEY |
+| inventory | INT | DEFAULT = 0 |
+
+```sql
+CREATE TABLE products (
+  product_ID INT PRIMARY KEY,
+  product_name VARCHAR(50) NOT NULL,
+  FK_genre_id INT,
+  rating INT DEFAULT 0,
+  price FLOAT NOT NULL,
+  FK_supplier_ID INT,
+  inventory INT DEFAULT 0,
+  FOREIGN KEY (FK_genre_id) REFERENCES genres(genre_id),
+  FOREIGN KEY (FK_supplier_ID) REFERENCES suppliers(supplier_ID)
+);
+```
+
+### Orders
+| Field | Type | Constraints |
+| ----- | ---- | ----------- |
+| order_ID | INT | PRIMARY KEY |
+| FK_customer_ID | INT | FOREIGN KEY |
+| FK_product_ID | INT | FOREIGN KEY |
+| quantity | INT | DEFAULT = 1  |
+| transaction_amount | FLOAT | NOT NULL |
+| transaction_date | DATE | NOT NULL |
+
+```sql
+CREATE TABLE orders (
+  order_ID INT PRIMARY KEY,
+  FK_customer_ID INT,
+  FK_product_ID INT,
+  quantity INT DEFAULT 1,
+  transaction_amount FLOAT NOT NULL,
+  transaction_date DATE NOT NULL,
+  FOREIGN KEY (FK_customer_ID) REFERENCES customers(customer_ID),
+  FOREIGN KEY (FK_product_ID) REFERENCES products(product_ID)
+);
+```
+
+### Suppliers
+| Field | Type | Constraints |
+| ----- | ---- | ----------- |
+| supplier_ID | INT | PRIMARY KEY | 
+| supplier_name | VARCHAR(50) | NOT NULL |
+| supplier_location | VARCHAR(50) | NOT NULL |
+
+```sql
+CREATE TABLE suppliers (
+  supplier_ID INT PRIMARY KEY,
+  supplier_name VARCHAR(50) NOT NULL,
+  supplier_location VARCHAR(50) NOT NULL
+);
+```
+
+### Genres
+| Field | Type | Constraints |
+| ----- | ---- | ----------- |
+| genre_id | INT | PRIMARY KEY |
+| genre_name | VARCHAR(50) | NOT NULL |
+
+```sql
+CREATE TABLE genres (
+  genre_id INT PRIMARY KEY,
+  genre_name VARCHAR(50) NOT NULL
+);
+```
---
+++ day-10/games-shop-design/games_shop.sql
@@ -0,0 +1,163 @@
+-- MySQL dump 10.13  Distrib 8.0.33, for Win64 (x86_64)
+--
+-- Host: localhost    Database: games_shop
+-- ------------------------------------------------------
+-- Server version	8.0.33
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `customers`
+--
+
+DROP TABLE IF EXISTS `customers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `customers` (
+  `customer_ID` int NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `phone_number` varchar(50) DEFAULT NULL,
+  `pwd` binary(20) NOT NULL,
+  `salt` binary(20) NOT NULL,
+  PRIMARY KEY (`customer_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `customers`
+--
+
+LOCK TABLES `customers` WRITE;
+/*!40000 ALTER TABLE `customers` DISABLE KEYS */;
+/*!40000 ALTER TABLE `customers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `genres`
+--
+
+DROP TABLE IF EXISTS `genres`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `genres` (
+  `genre_id` int NOT NULL,
+  `genre_name` varchar(50) NOT NULL,
+  PRIMARY KEY (`genre_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `genres`
+--
+
+LOCK TABLES `genres` WRITE;
+/*!40000 ALTER TABLE `genres` DISABLE KEYS */;
+/*!40000 ALTER TABLE `genres` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `orders`
+--
+
+DROP TABLE IF EXISTS `orders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `orders` (
+  `order_ID` int NOT NULL,
+  `FK_customer_ID` int DEFAULT NULL,
+  `FK_product_ID` int DEFAULT NULL,
+  `quantity` int DEFAULT '1',
+  `transaction_amount` float NOT NULL,
+  `transaction_date` date NOT NULL,
+  PRIMARY KEY (`order_ID`),
+  KEY `FK_customer_ID` (`FK_customer_ID`),
+  KEY `FK_product_ID` (`FK_product_ID`),
+  CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`FK_customer_ID`) REFERENCES `customers` (`customer_ID`),
+  CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`FK_product_ID`) REFERENCES `products` (`product_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `orders`
+--
+
+LOCK TABLES `orders` WRITE;
+/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `products`
+--
+
+DROP TABLE IF EXISTS `products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `products` (
+  `product_ID` int NOT NULL,
+  `product_name` varchar(50) NOT NULL,
+  `FK_genre_id` int DEFAULT NULL,
+  `rating` int DEFAULT '0',
+  `price` float NOT NULL,
+  `FK_supplier_ID` int DEFAULT NULL,
+  `inventory` int DEFAULT '0',
+  PRIMARY KEY (`product_ID`),
+  KEY `FK_genre_id` (`FK_genre_id`),
+  KEY `FK_supplier_ID` (`FK_supplier_ID`),
+  CONSTRAINT `products_ibfk_1` FOREIGN KEY (`FK_genre_id`) REFERENCES `genres` (`genre_id`),
+  CONSTRAINT `products_ibfk_2` FOREIGN KEY (`FK_supplier_ID`) REFERENCES `suppliers` (`supplier_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `products`
+--
+
+LOCK TABLES `products` WRITE;
+/*!40000 ALTER TABLE `products` DISABLE KEYS */;
+/*!40000 ALTER TABLE `products` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `suppliers`
+--
+
+DROP TABLE IF EXISTS `suppliers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `suppliers` (
+  `supplier_ID` int NOT NULL,
+  `supplier_name` varchar(50) NOT NULL,
+  `supplier_location` varchar(50) NOT NULL,
+  PRIMARY KEY (`supplier_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `suppliers`
+--
+
+LOCK TABLES `suppliers` WRITE;
+/*!40000 ALTER TABLE `suppliers` DISABLE KEYS */;
+/*!40000 ALTER TABLE `suppliers` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-07-14 16:14:22
