0e4db889f7f61af511ed388faacea272d0a0adf8
1391819 <roberto.nacu@gmail.com>
2023-07-18 12:22:46 +0100

    added collections tutorial and exercise

---
+++ day-12/collections/dictionary.py
@@ -0,0 +1,8 @@
+books = {
+    "The Handmaiden's Tale": "Margaret Atwood",
+    "The Hobbit": "J.R.R. Tolkien",
+    "Charlie and the Chocolate Factory": "Roald Dahl",
+}
+
+# remember to select key, not value
+print(books["The Handmaiden's Tale"])
---
+++ day-12/collections/exercise.py
@@ -0,0 +1,28 @@
+def get_books(author):
+    # this is local to the function
+    # shouldn't be here in the long term?
+    books_and_authors = {
+        "J.R.R. Tolkien": ["Lord of the Rings"],
+        "J.K. Rowling": [
+            "Harry Potter 1",
+            "Harry Potter 2",
+            "The Hobbit",
+        ],
+        "George Orwell": ["Animal Farm", "1984"],
+    }
+
+    # use entered author and return all his/her books as a list
+    books = books_and_authors.get(author, [])
+
+    return books
+
+
+# get user input
+author = input("Enter author name: ")
+
+# get needed books and sort
+books = sorted(get_books(author))
+
+# should add a check to see if there are any books
+to_return = ", ".join(books)
+print(to_return)
