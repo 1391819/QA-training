3982218f98eec569c0c1108c2c18c11ee90e7e29
1391819 <roberto.nacu@gmail.com>
2023-07-18 11:22:51 +0100

    added int-floats notes

--- day-12/intro/int-floats.py
+++ day-12/intro/int-floats.py
@@ -4,12 +4,15 @@ number2 = input("Enter decimal number: ")
 integer_number = int(number1)
 float_number = float(number2)
 # is the below int cast really needed?
-round_number = int(round(float_number))  # old
+# in older versions of python the first one
+# was needed since the number wouldn't be
+# converted to an int
+# round_number = int(round(float_number))  # old
 round_number2 = round(float_number)  # new
 
 print(number1)  # int
 print(number2)  # float
-print(type(round_number))  # debug
-print(type(round_number2))
-print(round_number)  # rounded float
+# print(type(round_number))  # debug
+print(type(round_number2))  # debug
+# print(round_number)  # rounded float
 print(round_number2)  # rounded float
