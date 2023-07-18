5c7a6ddd0b16954883055dcf05ed9f1be6c3d86a
1391819 <roberto.nacu@gmail.com>
2023-07-18 16:11:33 +0100

    added comments

--- day-12/w3s-practice/6.py
+++ day-12/w3s-practice/6.py
@@ -7,11 +7,15 @@ odd_count = 0
 # this could be turned into an user input
 numbers_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
+# go through all numbers in the list
 for number in numbers_list:
+    # if even, increase even counter
     if number % 2 == 0:
         even_count += 1
+    # else (if odd), increase odd counter
     else:
         odd_count += 1
 
+# print number of odd and even numbers
 print(f"Amount of even numbers in the list is {even_count}")
 print(f"Amount of odd numbers in the list is {odd_count}")
