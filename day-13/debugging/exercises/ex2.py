730f24ded6bc94a8209b14e24d9ea13678fe7088
1391819 <roberto.nacu@gmail.com>
2023-07-19 14:23:57 +0100

    added ex2 debugging practice

---
+++ day-13/debugging/exercises/ex2.py
@@ -0,0 +1,13 @@
+def product(n):
+    # from == to =, it's assignment
+    total = 1
+
+    # for loop
+    for i in n:
+        total *= i
+
+    # return within function
+    return total
+
+
+print(product([4, 4]))
