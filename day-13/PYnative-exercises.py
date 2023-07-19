37ed56d4906b29880b93ac04438a30b3d747ba36
1391819 <roberto.nacu@gmail.com>
2023-07-19 15:49:19 +0100

    added more exercises

--- day-13/PYnative-exercises.py
+++ day-13/PYnative-exercises.py
@@ -30,6 +30,37 @@ def calculate_cubes(n):
     return tmp_dict
 
 
+# nested functions
+# outer -> accept two parameters
+# inner -> addition between two parameters
+# outer -> add 5 to addition and return it
+def outer_function(a, b):
+    # parameters within the inner function can also be ommited
+    # since they are global to everything within the outer function
+
+    # this function is available within the outer faction now
+    def inner_function():
+        return a + b
+
+    # we call the inner function from within the outer one
+    sum = inner_function()
+
+    # add 5 and return
+    return sum + 5
+
+
+# calculate sum of numbers
+def recursive_function(n):
+    # need to add check in order to not encounter
+    # RecursionError: maximum recursion depth exceeded
+    # could be done within a try catch block and handle it properly?
+    # I'm not too familiar with recursive things
+    if n:
+        return n + recursive_function(n - 1)
+    else:
+        return 0
+
+
 if __name__ == "__main__":
     """
     # Check palindrome number
@@ -65,7 +96,30 @@ if __name__ == "__main__":
 
     # ------------------------------------------------------
 
+    """
+    # Nested functions
+    #
+    # Create an inner function to calculate the addition
+    # in the following way
+    #
+    # - Create an outer function that will accept two parameters, a and b
+    # - Create an inner function inside an outer function that will calculate the addition of a and b
+    # - At last, an outer function will add 5 into addition and return it
+    a = 1
+    b = 1
+    # we just need to call the outer function to be fair
+    addition = outer_function(a, b)
+    print(f"Sum + 5 using nested functions is: {addition}")
+    """
+
     # ------------------------------------------------------
+
+    # Recursive function
+    #
+    # Calculate the sum of numbers from 0 to n
+    n = 10
+    sum = recursive_function(n)
+    print(f"Sum using recursive function is: {sum}")
 
     # ------------------------------------------------------
 
