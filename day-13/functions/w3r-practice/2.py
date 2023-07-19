b5594144a4cfa323978afc25d11955c0b11be168
1391819 <roberto.nacu@gmail.com>
2023-07-19 10:29:31 +0100

    added ex2 functions practice

---
+++ day-13/functions/w3r-practice/2.py
@@ -0,0 +1,10 @@
+# https://www.w3resource.com/python-exercises/python-functions-exercises.php
+
+
+def sum_numbers(list):
+    return sum(list)
+
+
+numbers_list = [1, 2, 3, 4, 5]
+sum = sum_numbers(numbers_list)
+print(f"Sum is {sum}")
