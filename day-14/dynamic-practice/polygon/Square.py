a3a00016f0f08404fedd65d82c1e9a808d5ad669
1391819 <roberto.nacu@gmail.com>
2023-07-20 11:33:42 +0100

    added some comments

--- day-14/dynamic-practice/polygon/Square.py
+++ day-14/dynamic-practice/polygon/Square.py
@@ -1,6 +1,8 @@
+# imports
 from Rectangle import Rectangle
 
 
 class Square(Rectangle):
     def __init__(self, width):
+        # we call the init of the parent Class (Rectangle)
         super().__init__(width, width)
