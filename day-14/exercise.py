b4c5e33b939eead510010cf436baa009aff66bd6
1391819 <roberto.nacu@gmail.com>
2023-07-20 10:45:01 +0100

    just practicing more things

--- day-14/exercise.py
+++ day-14/exercise.py
@@ -13,16 +13,37 @@ class Class_:
         self.class_name = class_name
         self.student_list = student_list
 
-    def get_avg_test_score(self, score1, score2, score3):
-        return (score1 + score2 + score3) / 3
+    def get_avg_test_score(self):
+        for student in self.student_list:
+            print(
+                f"Student name: {student.name}, AVG Test Score: {student.avg_test_score}"
+            )
 
     def get_all_students(self):
         for student in self.student_list:
-            print(student.name)
+            print(f"Student: {student.name}")
 
 
-students_list = [Student("Mary", "18"), Student("John", "19"), Student("Mark", "22")]
+# students
+Mary = Student("Mary", "18")
+John = Student("John", "19")
+Mark = Student("Mark", "22")
 
+# students list
+students_list = [Mary, John, Mark]
+
+# creating class
+# this could be done student by student rather than using a list
 new_class = Class_("Comp Sci", students_list)
 
+# print all students
 new_class.get_all_students()
+
+# we calculate avg_test_score for one student first
+# we use the class list, not the original one!
+new_class.student_list[0].score1 = 10
+new_class.student_list[0].score2 = 10
+new_class.student_list[0].score3 = 10
+new_class.student_list[0].get_avg_test_score()
+
+new_class.get_avg_test_score()
--- day-14/tutorial.py
+++ day-14/tutorial.py
@@ -2,10 +2,18 @@ class Student:
     def __init__(self, name, age):
         self.name = name
         self.age = age
+        self.avg_test_score = 0
+        self.score1 = 0
+        self.score2 = 0
+        self.score3 = 0
+
+    def get_avg_test_score(self):
+        self.avg_test_score = (self.score1 + self.score2 + self.score3) / 3
+        return self.avg_test_score
 
 
-John = Student("John", "21")
-Jane = Student("Jane", "22")
+# John = Student("John", "21")
+# Jane = Student("Jane", "22")
 
-print(getattr(John, "age"))
+# print(getattr(John, "age"))
 # print(John.age)
