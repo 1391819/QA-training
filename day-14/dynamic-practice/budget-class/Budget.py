9797bf25c14502eac71580ff6c037bc8cb4dc1fd
1391819 <roberto.nacu@gmail.com>
2023-07-21 13:51:21 +0100

    added some extra comments

--- day-14/dynamic-practice/budget-class/Budget.py
+++ day-14/dynamic-practice/budget-class/Budget.py
@@ -1,27 +1,36 @@
 class Budget:
     def __init__(self, category, fund=0):
-        self.category = category
-        self.fund = fund
+        self.category = category  # category name
+        self.fund = fund  # category fund
 
+    # withdraw specific amount from fund
     def withdraw(self, amount):
+        # checking if it's possible to withdraw funds
         if self.fund < amount:
             print(f"Not enough funds")
         else:
             self.fund -= amount
+            # printing new status
             print(f"Withdrawn {amount} from {self.category}")
             print(f"New fund is {self.fund}")
 
+    # deposit specific amount to fund
     def deposit(self, amount):
         self.fund += amount
+        # printing new status
         print(f"Deposited {amount} in {self.category}")
         print(f"New fund is {self.fund}")
 
+    # transferring funds from one category to another one
+    # other_category is a different Budget instance
     def transfer(self, amount, other_category):
+        # checking if it's possible to withdraw funds
         if self.fund < amount:
             print(f"Not enough funds")
         else:
-            self.fund -= amount
-            other_category.fund += amount
+            self.fund -= amount  # withdraw from one category
+            other_category.fund += amount  # deposit into the other
+            # printing new status
             print(
                 f"{amount} was withdrawn from {self.category} to {other_category.category}"
             )
--- day-14/dynamic-practice/budget-class/app.py
+++ day-14/dynamic-practice/budget-class/app.py
@@ -1,6 +1,8 @@
 """
 
-Goal: “Create a Budget class that can instantiate objects based on different budget categories like food, clothing, and entertainment. These objects should allow for depositing and withdrawing funds from each category, as well computing category balances and transferring balance amounts between categories”
+Goal:
+
+Create a Budget class that can instantiate objects based on different budget categories like food, clothing, and entertainment. These objects should allow for depositing and withdrawing funds from each category, as well computing category balances and transferring balance amounts between categories
 
 """
 
@@ -8,18 +10,21 @@ Goal: “Create a Budget class that can instantiate objects based on different b
 from Budget import Budget
 
 if __name__ == "__main__":
-    # Budget class
+    # categories
     food = Budget("Food", 50)
     clothing = Budget("Clothing", 100)
     entertainment = Budget("Entertainment", 150)
 
+    # printing categories and their funds
     print(f"{food.category} fund is {food.fund}")
     print(f"{clothing.category} fund is {clothing.fund}")
     print(f"{clothing.category} fund is {clothing.fund}")
-    print()
+    print()  # new line
 
+    # withdrawing and depositing methods
     food.withdraw(5000)
     clothing.deposit(50)
     entertainment.deposit(100)
 
+    # transferring funds from one category to the other
     food.transfer(5, clothing)
