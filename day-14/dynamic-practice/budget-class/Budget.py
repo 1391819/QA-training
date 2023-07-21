1fdbb1b600380492c2f6a806a5a6450c4525b7fe
1391819 <roberto.nacu@gmail.com>
2023-07-21 14:40:52 +0100

    removed code duplication

--- day-14/dynamic-practice/budget-class/Budget.py
+++ day-14/dynamic-practice/budget-class/Budget.py
@@ -9,11 +9,13 @@ class Budget:
         # checking if it's possible to withdraw funds
         if self.fund < amount:
             print(f"Not enough funds")
+            return False
         else:
             self.fund -= amount
             # printing new status
             print(f"Withdrawn {amount} from {self.category}")
             print(f"New fund is {self.fund}")
+            return True
 
     # deposit specific amount to fund
     def deposit(self, amount):
@@ -25,15 +27,11 @@ class Budget:
     # transferring funds from one category to another one
     # other_category is a different Budget instance
     def transfer(self, amount, other_category):
-        # checking if it's possible to withdraw funds
-        if self.fund < amount:
-            print(f"Not enough funds")
-        else:
-            self.fund -= amount  # withdraw from one category
-            other_category.fund += amount  # deposit into the other
-            # printing new status
-            print(
-                f"{amount} was withdrawn from {self.category} to {other_category.category}"
-            )
-            print(f"{self.category} fund is {self.fund}")
-            print(f"{other_category.category} fund is {other_category.fund}")
+        self.withdraw(amount)  # no code duplication
+        other_category.deposit(amount)
+        # printing new status
+        print(
+            f"{amount} was withdrawn from {self.category} to {other_category.category}"
+        )
+        print(f"{self.category} fund is {self.fund}")
+        print(f"{other_category.category} fund is {other_category.fund}")
