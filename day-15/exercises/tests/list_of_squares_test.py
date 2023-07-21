5ec3cb25b1e46869b3fbb85ea1b5bb4071f7cf2f
1391819 <roberto.nacu@gmail.com>
2023-07-21 11:09:49 +0100

    added extra assert

--- day-15/exercises/tests/list_of_squares_test.py
+++ day-15/exercises/tests/list_of_squares_test.py
@@ -3,5 +3,6 @@ from programs import list_of_squares
 
 # return a list of n squares in a dict
 def test_list_of_squares():
+    assert list_of_squares.list_of_squares(0) == {}
     assert list_of_squares.list_of_squares(1) == {1: 1}
     assert list_of_squares.list_of_squares(3) == {1: 1, 2: 4, 3: 9}
