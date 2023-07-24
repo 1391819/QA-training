dcd6a27a93a50f574bec1e5a4fcf43b2a3d9c703
1391819 <roberto.nacu@gmail.com>
2023-07-24 12:20:19 +0100

    added gui example #2

---
+++ day-16/gui/gui_example_2.py
@@ -0,0 +1,36 @@
+# imports
+import tkinter
+
+
+def strip_vowels() -> None:
+    """Strip vowels from an input string taken via the root"""
+    global label
+    # destroy any existing labels on the screen
+    label.destroy()
+    # get text from input
+    s = input.get()
+    # create new label
+    label = tkinter.Label(
+        root, text="".join(char for char in s if char not in "aeiouAEIOU")
+    )
+    # place label
+    label.grid(row=2, column=1)
+
+
+# declare root of our GUI - window object
+root = tkinter.Tk()
+
+# label
+label = tkinter.Label(root, text="")
+
+# textbox
+input = tkinter.Entry(root)
+input.grid(row=1, column=1)
+
+# submit button
+submit = tkinter.Button(root, text="Strip vowels", command=strip_vowels)
+submit.grid(row=1, column=2)
+
+if __name__ == "__main__":
+    # run GUI - main event loop
+    root.mainloop()
