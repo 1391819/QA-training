ebe232452e977f8332a08bb401a3f3fce73ff4b4
1391819 <roberto.nacu@gmail.com>
2023-07-24 19:07:25 +0100

    fixed buggy probability computation

--- day-14/dynamic-practice/lottery-ball/Hat.py
+++ day-14/dynamic-practice/lottery-ball/Hat.py
@@ -4,13 +4,12 @@ import random
 
 # Hat class
 class Hat:
-    # init
     # balls -> collects all keyword arguments and packs them into a dict
     #          keys = colour names and values = number of balls
     def __init__(self, **balls):
-        self.colours = list(balls.keys())
-        self.count = list(balls.values())
-        self.total_balls = sum(self.count)
+        self.colours = list(balls.keys())  # all present colours in the hat
+        self.count = list(balls.values())  # number of balls in the hat
+        self.total_balls = sum(self.count)  # total number of balls in the hat
         self.balls = self.unpack()  # unpacking balls dict to a list
 
     # unpack balls dict to a list
@@ -40,33 +39,37 @@ class Hat:
 
         return drawn_balls
 
-    # TODO: Fix it, it's still buggy
-    # compute probability of picking a particular
-    # distribution of balls over a certain amount of experiments
+    # compute probability of picking a particular distribution of balls over a certain amount of experiments
     def compute_probability(self, num_experiments, distribution):
         # amount of times the desired distribution was found
         success_count = 0
 
-        # check to prevent computations if colour not present in hat
-        if not all(color in self.colours for color in distribution.keys()):
+        # check that the hat contains the distribution colours
+        distribution_colours = list(distribution.keys())  # get "asked" colours
+        if not all(colour in self.colours for colour in distribution_colours):
             raise ValueError("The distribution must specify valid colours in the hat")
 
+        # have a temporary hat to perform experiments - the same object could be reused but for simplicity
+        # I will do it this way
+        tmp_hat_balls = self.balls.copy()
+        tmp_hat_colours = self.colours.copy()  # all present colours in the tmp hat
+        tmp_hat_total_balls = self.total_balls  # total number of balls in the tmp hat
+
         for _ in range(num_experiments):
-            # have a temporary hat to perform experiments
-            hat_copy = self.balls.copy()
-            # list of drawn balls
-            drawn_balls = self.pick_balls()
+            # number of balls to draw
+            n_balls_to_draw = random.randint(1, tmp_hat_total_balls)
+            # balls that were drawn
+            # we do not remove them from the hat since we are just trying to compute the
+            # probability
+            # if we'd want to remove them from the hat, we'd have to handle possible valueerrors
+            drawn_balls = random.sample(tmp_hat_balls, n_balls_to_draw)
+
             # check count for each colour - make dict of drawn distribution
             drawn_balls_counts = {
-                color: drawn_balls.count(color) for color in self.colours
+                colour: drawn_balls.count(colour) for colour in tmp_hat_colours
             }
             # if picked distribution is the one we want, update success count
             if drawn_balls_counts == distribution:
                 success_count += 1
-                # print("I'm here")  # debug
-
-            # updating
-            self.balls = hat_copy
-            self.total_balls = len(self.balls)
 
         return success_count / num_experiments
--- day-14/dynamic-practice/lottery-ball/app.py
+++ day-14/dynamic-practice/lottery-ball/app.py
@@ -12,24 +12,20 @@ Give the object the ability to pick a random number of balls from the hat, which
 from Hat import Hat
 
 if __name__ == "__main__":
-    """Main"""
-
     # creating Hat instance and adding different balls
-    # no need to give a dict, we make use of Python's keyword args
-    hat = Hat(red=3, yellow=5, black=7, pink=2)
+    hat = Hat(red=50, yellow=50)
 
     # pick a random number of balls
     drawn_balls = hat.pick_balls()
     # print picked balls
-    # print(f"Drawn balls: {drawn_balls}")
+    print(f"Drawn balls: {drawn_balls}")
 
     # compute probability of picking a particular
     # distribution of balls over a certain amount of experiments
-    num_experiments = 5000
+    num_experiments = 500
     distribution = {
-        "red": 2,
-        "yellow": 1,
-        "pink": 1,
+        "red": 25,
+        "yellow": 25,
     }
     probability = hat.compute_probability(num_experiments, distribution)
     # : -> start of string formatting (float numbers)
