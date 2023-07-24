abf31b5dd0ed1ea8194c4e7d5e0ae30b0327363a
1391819 <roberto.nacu@gmail.com>
2023-07-24 11:29:01 +0100

    refactoring - added docstrings

--- day-14/exercise.py
+++ day-14/exercise.py
@@ -5,44 +5,61 @@ It should contain the following attributes: name, age, and class with default va
 It should also contain a method which takes in 3 test scores and prints the students average test score.
 """
 
+# imports
 from tutorial import Student
 
 
 class Class_:
-    def __init__(self, class_name, student_list):
+    """Class to manage a group of students, made using Student instances"""
+
+    def __init__(self, class_name: str, student_list: Student) -> None:
+        """Initialise Class instance
+
+        Args:
+            class_name (str): Name of the class
+            student_list (Student): List of Student instances
+        """
         self.class_name = class_name
         self.student_list = student_list
 
-    def get_avg_test_score(self):
-        for student in self.student_list:
-            print(
-                f"Student name: {student.name}, AVG Test Score: {student.avg_test_score}"
-            )
+    def get_avg_test_score(
+        self, student: Student, score1: float, score2: float, score3: float
+    ) -> None:
+        """Return average test score of a particular student
 
-    def get_all_students(self):
+        Args:
+            student (Student): Student to compute the average test score of
+            score1 (float): Score of test 1
+            score2 (float): Score of test 2
+            score3 (float): Score of test 3
+        """
+        print(
+            f"Student name: {student.name}, AVG Test Score: {student.compute_avg_test_score(score1, score2, score3)}"
+        )
+
+    def get_all_students(self) -> None:
+        """Print all students"""
         for student in self.student_list:
             print(f"Student: {student.name}")
 
 
-# students
-Mary = Student("Mary", "18")
-John = Student("John", "19")
-Mark = Student("Mark", "22")
+if __name__ == "__main__":
+    # students
+    Mary = Student("Mary", "18")
+    John = Student("John", "19")
+    Mark = Student("Mark", "22")
 
-# students list
-students_list = [Mary, John, Mark]
+    # students list
+    students_list = [Mary, John, Mark]
 
-# creating class
-# this could be done student by student rather than using a list
-new_class = Class_("Comp Sci", students_list)
+    # creating class
+    # this could be done student by student rather than using a list
+    new_class = Class_("Comp Sci", students_list)
 
-# print all students
-new_class.get_all_students()
+    # print all students
+    new_class.get_all_students()
 
-# we calculate avg_test_score for one student first
-# we use the class list, not the original one!
-new_class.student_list[0].score1 = 10
-new_class.student_list[0].score2 = 10
-new_class.student_list[0].score3 = 10
-
-new_class.get_avg_test_score()
+    # calculate avg_test_score
+    new_class.get_avg_test_score(Mary, 10, 10, 10)
+    new_class.get_avg_test_score(John, 20, 20, 20)
+    new_class.get_avg_test_score(Mark, 40, 40, 40)
--- day-14/tutorial.py
+++ day-14/tutorial.py
@@ -1,19 +1,31 @@
 class Student:
-    def __init__(self, name, age):
+    """
+    Student class used to track name, age, and the average test score
+    """
+
+    def __init__(self, name: str, age: int) -> None:
+        """Initialise student instance
+
+        Args:
+            name (str): Name of the student
+            age (int): Age of the student
+        """
         self.name = name
         self.age = age
         self.avg_test_score = 0
-        self.score1 = 0
-        self.score2 = 0
-        self.score3 = 0
 
-    def get_avg_test_score(self):
-        self.avg_test_score = (self.score1 + self.score2 + self.score3) / 3
+    def compute_avg_test_score(
+        self, score1: float, score2: float, score3: float
+    ) -> None:
+        """Compute the student average test score
+
+        Args:
+            score1 (float): Score of test 1
+            score2 (float): Score of test 2
+            score3 (float): Score of test 3
+
+        Returns:
+            float: Average of test scores
+        """
+        self.avg_test_score = (score1 + score2 + score3) / 3
         return self.avg_test_score
-
-
-# John = Student("John", "21")
-# Jane = Student("Jane", "22")
-
-# print(getattr(John, "age"))
-# print(John.age)
