4f92394d9b1c1f4c4fa1fbdb8adafb8585459f9e
1391819 <roberto.nacu@gmail.com>
2023-07-24 12:23:49 +0100

    added window sizing and initial positioning

--- day-16/gui/gui_example_2.py
+++ day-16/gui/gui_example_2.py
@@ -19,6 +19,21 @@ def strip_vowels():
 # declare root of our GUI - window object
 root = tkinter.Tk(className="Vowel Remover")
 
+w = 800  # width for the Tk root
+h = 650  # height for the Tk root
+
+# get screen width and height
+ws = root.winfo_screenwidth()  # width of the screen
+hs = root.winfo_screenheight()  # height of the screen
+
+# calculate x and y coordinates for the Tk root window
+x = (ws / 2) - (w / 2)
+y = (hs / 2) - (h / 2)
+
+# set the dimensions of the screen
+# and where it is placed
+root.geometry("%dx%d+%d+%d" % (w, h, x, y))
+
 # label
 label = tkinter.Label(root, text="")
 
