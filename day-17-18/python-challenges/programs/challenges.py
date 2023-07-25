f29705261d36f51936bd37991dc8cd5cb9c5bab5
1391819 <roberto.nacu@gmail.com>
2023-07-25 16:30:19 +0100

    upadted challenge 2 instructions

--- day-17-18/python-challenges/programs/challenges.py
+++ day-17-18/python-challenges/programs/challenges.py
@@ -5,40 +5,51 @@
 # - returns the string in all upper case if its' length is even, and all lower case if its' length is odd. Any non-alphabetic characters should not be changed
 # - stretch goal: try to implement this both with and without the .upper()/.lower() methods
 
+
 def func_1_methods(string):
     pass
+
 
 def func_1_no_methods(string):
     pass
 
+
 # Challenge 2: write a function which:
 # - takes a word and a list of words as input
+# - if words are same length - we skip (continue)
 # - returns a new list containing only the words from the given list which differ from the given word in exactly one position
 # You should ignore case
 
+
 def func_2(string, list):
     pass
+
 
 # Challenge 3: write a function which:
 # - takes two ints and a list of ints as arguments
 # - returns a list of bools where the ith element is True if list[i] has the two given ints as factors and false otherwise
 
+
 def func_3(int1, int2, intlist):
     pass
+
 
 # Challenge 4: write a function which:
 # - takes a string as an argument
 # - returns a new string containing only the letters from the original string which are at an even-numbered position in the alphabet (letting a = 1, b = 2, ...)
 # - the returned string should only contain letter characters. All chars in the returned string should be lower case
 
+
 def func_4(string):
     pass
+
 
 # Challenge 5: write a function which:
 # - takes a list of integers as argument
 # - for each int, creates a list of the factors of the number, including 1 but excluding the number itself
 # - converts these lists to strings and writes them out to a csv file called factors.csv, each on a new line
 # - note: this function should be a void (i.e return nothing)
+
 
 def func_5(list):
     pass
