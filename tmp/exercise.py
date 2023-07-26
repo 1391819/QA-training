4a3dd04f88ce4c030a2030d7bf7817c0d1765545
1391819 <roberto.nacu@gmail.com>
2023-07-26 11:20:14 +0100

    added simple Flask exercise

---
+++ tmp/exercise.py
@@ -0,0 +1,27 @@
+"""
+
+Write a random name generator which allows a user to generate a random firstname + lastname combination
+
+"""
+from random import randint
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    name = random_generator()
+
+    return name
+
+
+def random_generator():
+    first_name_list = ["Roberto", "Amy", "Sarah", "Mark", "John"]
+    last_name_list = ["Nacu", "Robinsons", "Smith", "Johnson"]
+
+    return f"{first_name_list[randint(0, len(first_name_list) - 1)]} {last_name_list[randint(0, len(last_name_list) - 1)]}"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
