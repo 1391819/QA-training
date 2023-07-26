ad39348223b211f976adb4aa6097595ee68550bc
1391819 <roberto.nacu@gmail.com>
2023-07-26 09:22:10 +0100

    added practice day 2 classes exercise template

---
+++ day-17-18/classes-and-objects-2/clock.py
@@ -0,0 +1,26 @@
+# https://github.com/agray998/oop-exercise-2
+
+
+"""
+Task: create a clock class. Each clock should have the attributes hours, minutes and seconds.
+The class should have methods to:
+ - initialise these attributes upon instance creation
+ - update the clock's time by one second when called (remembering to wrap appropriately after every 60 seconds)
+ - return a string representation of the current clock time
+ Create a subclas of this class for 12-hour clocks, and override the methods appropriately
+"""
+
+
+class Clock:
+    def __init__(self, hours=0, minutes=0, seconds=0):
+        ...
+
+    def tick(self):
+        ...
+
+    def __str__(self):
+        ...
+
+
+class TwelveHourClock(Clock):
+    ...
