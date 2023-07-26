27d8467d8b02e90c3a438d0e323f70156fa8a2d4
1391819 <roberto.nacu@gmail.com>
2023-07-26 13:54:07 +0100

    added exercise template

---
+++ tmp/full-practice/run.py
@@ -0,0 +1,9 @@
+"""
+Using the solution to the OOP exercise from yesterday (the book class), 
+create a flask app which allows users to:
+
+- add new books with information supplied via url/query params
+- search for books by a given author
+- update books that have already been added (stretch goal)
+- delete books from the system (stretch goal)
+"""
