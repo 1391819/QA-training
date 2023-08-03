60ed5785672422ec4c4478e531c5a74f36f1addd
1391819 <roberto.nacu@gmail.com>
2023-08-03 12:11:44 +0100

    removed conditional extra exercise

--- day-21-25/js/iteration.js
+++ day-21-25/js/iteration.js
@@ -42,45 +42,10 @@
 
 // Ex 3
 
-// for (let j = 1; j < 11; j++) {
-// 	for (let k = 0; k < 10; k++) {
-// 		console.log(j);
-// 	}
-// }
-
-// ------------------------------------
-
-/*
-
-Write a switch case statement which uses the current day as its expression and matches with the relevant case. 
-
-Criteria:
-    - Omit a break statement if it is a weekday, until the last day
-    - Use a default case to handle an invalid range.
-
-*/
-
-let day = new Date().getDay();
-// console.log(day); // debug
-
-switch (day) {
-	case 1:
-		console.log('Monday');
-	case 2:
-	case 3:
-	case 4:
-	case 5:
-		console.log('Friday');
-		break;
-	case 6:
-		console.log('Saturday');
-		break;
-	case 7:
-		console.log('Sunday');
-		break;
-	default:
-		console.log('Invalid day');
-		break;
+for (let j = 1; j < 11; j++) {
+	for (let k = 0; k < 10; k++) {
+		console.log(j);
+	}
 }
 
 // ------------------------------------
