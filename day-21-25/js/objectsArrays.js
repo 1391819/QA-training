a6cdb3db49b5d22ead4cedaaa6c9424634612eeb
1391819 <roberto.nacu@gmail.com>
2023-08-04 11:31:08 +0100

    fixed typo and added alternative solutions

--- day-21-25/js/objectsArrays.js
+++ day-21-25/js/objectsArrays.js
@@ -30,8 +30,8 @@ Create the following log statements using the darthVader Object
 console.log(`Darth Vader's allegiance is to the ${darthVader.allegiance}`);
 console.log(`Darth Vader's weapon of choice is a ${darthVader.weapon}`);
 console.log(`Darth Vader' is a sith? ${darthVader.sith}`);
-console.log(`Darth Vader is a Jedi? ${darthVader.Jedi ? 'false' : 'true'}`);
-
+console.log(`Darth Vader is a Jedi? ${darthVader.sith ? 'false' : 'true'}`);
+console.log(`Darth Vader is a Jedi? ${!darthVader.sith}`);
 // ------------------------------------
 
 /*
